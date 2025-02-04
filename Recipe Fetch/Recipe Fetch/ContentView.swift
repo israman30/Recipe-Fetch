1b5e4570a798d2ed8a6ad00c58a5847f953ec6a5
Israel Manzo <israelmanzo814@gmail.com>
2025-02-03 21:57:50 -0500

    added network manager class

--- Recipe Fetch/Recipe Fetch/ContentView.swift
+++ Recipe Fetch/Recipe Fetch/ContentView.swift
@@ -7,6 +7,31 @@
 
 import SwiftUI
 
+class NetworkManager {
+    
+    private let urlCache: URLCache
+    
+    init(urlCache: URLCache = .shared) {
+        self.urlCache = urlCache
+    }
+    
+    func fetch<T: Decodable>(from url: URL) async throws -> T {
+        if let cacheResponse = urlCache.cachedResponse(for: URLRequest(url: url)) {
+            let decodeData = try JSONDecoder().decode(T.self, from: cacheResponse.data)
+            return decodeData
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        if let httpsResponse = response as? HTTPURLResponse, httpsResponse.statusCode == 200 {
+            let cacheResponse = CachedURLResponse(response: response, data: data)
+            urlCache.storeCachedResponse(cacheResponse, for: URLRequest(url: url))
+        }
+        
+        return try JSONDecoder().decode(T.self, from: data)
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
