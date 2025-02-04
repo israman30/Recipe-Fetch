980b10e5dbc3d32f91e961ed8f4234c38098da12
Israel Manzo <israelmanzo814@gmail.com>
2025-02-03 20:02:27 -0500

    added recipe project

---
+++ Recipe Fetch/Recipe Fetch/Recipe_FetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_FetchApp.swift
+//  Recipe Fetch
+//
+//  Created by Israel Manzo on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_FetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
