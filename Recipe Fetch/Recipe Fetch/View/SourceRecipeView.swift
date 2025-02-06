76d846ee66da8456d40f2d747fc75191bb2547b7
Israel Manzo <israelmanzo814@gmail.com>
2025-02-06 18:16:03 -0500

    added detail recipe

---
+++ Recipe Fetch/Recipe Fetch/View/SourceRecipeView.swift
@@ -0,0 +1,19 @@
+//
+//  SourceRecipeView.swift
+//  Recipe Fetch
+//
+//  Created by Israel Manzo on 2/6/25.
+//
+
+import SwiftUI
+
+struct SourceRecipeView: View {
+    
+    var body: some View {
+        EmptyView()
+    }
+}
+
+#Preview {
+    SourceRecipeView()
+}
