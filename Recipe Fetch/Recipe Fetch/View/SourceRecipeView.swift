4ef0ee3a6047d30f14a5bc6edf9cc1846f404ce3
Israel Manzo <israelmanzo814@gmail.com>
2025-02-06 18:35:20 -0500

    added source view body with source

--- Recipe Fetch/Recipe Fetch/View/SourceRecipeView.swift
+++ Recipe Fetch/Recipe Fetch/View/SourceRecipeView.swift
@@ -9,11 +9,48 @@ import SwiftUI
 
 struct SourceRecipeView: View {
     
+    var recipe: Recipe?
+    
     var body: some View {
-        EmptyView()
+        VStack {
+            AsyncImage(url: URL(string: recipe?.photoURLLarge ?? "")) { phase in
+                if let image = phase.image {
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .cornerRadius(5)
+                } else if phase.error != nil {
+                    Text("Error loading image.")
+                        .font(.title)
+                        .foregroundStyle(.gray)
+                } else {
+                    Image(systemName: "wifi.exclamationmark")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200)
+                }
+            }
+            VStack(alignment: .center) {
+                Text(recipe?.name ?? "")
+                    .font(.largeTitle)
+                    .fontWeight(.medium)
+                Text(recipe?.cuisine ?? "")
+                    .font(.title)
+            }
+            VStack {
+                if let url = URL(string: recipe?.sourceURL ?? "") {
+                    Link(destination: url) {
+                        Text("Visit Source")
+                            .shadowText()
+                    }
+                }
+            }
+            Spacer()
+        }
+        .ignoresSafeArea(.all)
     }
 }
 
 #Preview {
-    SourceRecipeView()
+    SourceRecipeView(recipe: Recipe(cuisine: "Malasyan", name: "Apam Balik", photoURLLarge: "https://d3jbb8n5wk0qxi.cloudfront.net/photos/b9ab0071-b281-4bee-b361-ec340d405320/large.jpg", photoURLSmall: "", sourceURL: "https://www.nyonyacooking.com/recipes/apam-balik~SJ5WuvsDf9WQ", uuid: "xxx"))
 }
